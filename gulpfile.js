aa5fa03f7b4025738747f47d0ae2d39fa5e5cce0
David <david.jimenez.lamas@alumnos.upm.es>
2018-08-05 09:51:13 +0200

    - Todo sincronizado con browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,45 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('default', ['css', 'javascript'], function() {
     browserSync.init({
-        server: "./app"
+        server: {
+            baseDir: ["./app/public",".app/public/css"]
+        }
     });
 
     gulp.watch("app/js/*.js", ["javascript"]).on('change', browserSync.reload);
-    gulp.watch("app/scss/**/*.scss", ['css']);
-    gulp.watch("public/html/*.html").on('change', browserSync.reload);
+    gulp.watch("app/scss/*.scss", ['css']).on('change', browserSync.reload);
     gulp.watch("app/html/*.html", ["html"]);
+    gulp.watch("app/html/*.html").on('change', browserSync.reload);
 });
+
+
 
 gulp.task('html', function() {
   return gulp.src('app/html/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('public/html'));
+    .pipe(gulp.dest('app/public'));
 });
 
 gulp.task('imagenes', function() {
     gulp.src('app/img/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('public/img'));
+        .pipe(gulp.dest('app/public/img'));
 });
 
 gulp.task('javascript', function() {
     gulp.src('app/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('public/js'));
+        .pipe(gulp.dest('app/public/js'));
 });
 
 gulp.task('css', function(){
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src('app/scss/*.scss')
         .pipe(sass())
         .pipe(cssnano())
         .pipe(autoprefixer({
             browsers: ['last 4 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('public/css'))
+        .pipe(gulp.dest('app/public/css'))
         .pipe(browserSync.stream());
 });
